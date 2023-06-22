1a8f02ef26a7cc7cd890e6e09e438637e78f5e64
Sydney Laub <syd.laub18@gmail.com>
2023-06-22 13:11:40 -0400

    pseudocoded logic for render user request in CL

--- index.js
+++ index.js
@@ -48,61 +48,69 @@ startApp();
 
 
 // create variables for each case that user could pick
-const viewDept = () => {
-
+const viewDeptartment = () => {
+    // WHEN I choose to view all departments
+    // THEN I am presented with a formatted table showing department names and department ids
+    
 };
 
 
 
 const viewRoles = () => {
+    // WHEN I choose to view all roles
+    // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+    
+};
+
+const viewEmployees = () => {
+    // WHEN I choose to view all employees
+    // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
 };
 
 
 
-const addDeptartment = () => {
 
+const addDeptartment = () => {
+    // WHEN I choose to add a department
+    // THEN I am prompted to enter the name of the department and that department is added to the database
+    
 };
 
 
 
 const addRole = () => {
-
+    // WHEN I choose to add a role
+    // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+    
 };
+
 
 
 const addEmployee = () => {
+    // WHEN I choose to add an employee
+    // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+    
+};
+
+
+
+const updateRole = () => {
+    // WHEN I choose to update an employee role
+    // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
 };
 
 
 
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
 
 
 
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-
-
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
 
 
 
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
 
 
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
 
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
